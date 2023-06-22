098caa063f344f9df20932a8de98e68c7dc2cf3c
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2023-06-21 20:35:05 -0400

    add graph Node class

---
+++ src/main/java/commonAlgorithms/graph/Node.java
@@ -0,0 +1,22 @@
+package commonAlgorithms.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        this.val = 0;
+        this.neighbors = new ArrayList<Node>();
+    }
+    public Node(int val) {
+        this.val = val;
+        this.neighbors = new ArrayList<Node>();
+    }
+    public Node(int val, ArrayList<Node> neighbors) {
+        this.val = val;
+        this.neighbors = neighbors;
+    }
+
+}
